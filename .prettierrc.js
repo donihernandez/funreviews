37e8105ede8ec82ba6d6b2475d559b050184f877
Adonai Dominguez Hernandez <donihernandez003@gmail.com>
2022-05-13 15:39:21 -0400

    feat(eslint-prettier): adding eslint, prettier and husky together

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	endOfLine: 'lf',
+	printWidth: 80,
+	trailingComma: 'es5',
+	semi: false,
+	jsxSingleQuote: true,
+	singleQuote: true,
+	useTabs: true,
+	tabWidth: 2,
+	arrowParens: 'always',
+}

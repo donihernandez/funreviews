fe5f4f038cb9dd4865b2003d5c0ef9cfe3c662ae
Adonai Dominguez Hernandez <donihernandez003@gmail.com>
2022-08-10 18:04:42 -0400

    feat(change-firebase-env): change the firebase setup in the env file

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyB3g0iTbjPCFDDDsUPzPdW281vEr_7RKfo',
-    appId: '1:514573735978:web:4d7657715bd28002954b53',
-    authDomain: 'fun-reviews-5cc40.firebaseapp.com',
-    measurementId: 'G-PLR5FVEWPV',
-    messagingSenderId: '514573735978',
-    projectId: 'fun-reviews-5cc40',
-    storageBucket: 'fun-reviews-5cc40.appspot.com',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 };
 
 const app = initializeApp(firebaseConfig);

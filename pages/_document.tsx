f69dac9f53dbedda89303ed99f7d87fadd5f0150
Adonai Domingez <donihernandez003@gmail.com>
2022-11-13 19:43:49 -0500

    feat(add-ga4): install GA4 Scripts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable max-len */
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
     render() {
@@ -18,6 +20,25 @@ class MyDocument extends Document {
                         // eslint-disable-next-line max-len
                         href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Nunito:wght@400;500;600;700;800&display=swap"
                         rel="stylesheet"
+                    />
+
+                    <Script
+                        async
+                        id="GA4"
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID}`}
+                    />
+                    <Script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                        }}
+                        id="GA4_TAG"
                     />
                 </Head>
                 <body>
